22293981a6426effb380b27ad1ded24e48cc848b
yujizcat2 <yujizcat@gmail.com>
2026-09-07 23:58:56 +0900

    Redesign Prime Game Gold main UI

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './styles.css';
 import './mobile.css';
+import './game-ui-refresh.css';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode><App /></React.StrictMode>,
